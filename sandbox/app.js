4ad477dc3d0f14c2181cb96e5e565c44a009378a
Simon Knee <s.knee@nhs.net>
2022-05-13 15:45:54 +0100

    DAW-1374-Test2: test sandbox 6

--- sandbox/app.js
+++ sandbox/app.js
@@ -128,7 +128,7 @@ app.get("/_status", handlers.status);
 app.get("/health", handlers.status);
 app.all("/hello", handlers.hello);
 app.all("/datasets", handlers.datasets);
-app.all("/datasets{id}", handlers.datasetsId);
+app.all("/datasets/{id}", handlers.datasetsId);
 app.use(on_error)
 app.use(after_request);
 
